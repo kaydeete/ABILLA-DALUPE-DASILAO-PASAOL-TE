d758a606ba19a14a2addbbfc8fce318a23e1cd19
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-03-26 19:52:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+tossCoin()
+{
+    var coin = Math.random();
+
+    if (coin == 0)
+    {
+        document.write("Coin chooses head");
+    }
+    else
+    {
+        document.write("Coin chooses tail");
+    }
+}
